d00d89c5bd3f56e18b12ea484c2e3ff6d34f9b61
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-21 21:52:32 +0530

    Considering buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputBtns = document.querySelectorAll(".inputBtn"),
+  input = document.querySelectorById("input"),
+  equal = document.querySelectorById("equal"),
+  clear = document.querySelectorById("clear"),
+  erase = document.querySelectorById("delete");
